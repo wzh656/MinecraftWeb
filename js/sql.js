b4aeecb70c5b150ba96962087b4c7b87ea8d0873
wzh656 <60345146+wzh656@users.noreply.github.com>
2020-07-21 14:09:52 +0800

    a

--- js/sql.js
+++ js/sql.js
@@ -1,81 +1,69 @@
-const SQL = {};
-//打开数据库
-SQL.openDatabase = function(dbName, version, decision, size){
-	SQL.db = openDatabase(dbName, version, decision, size);
-};
+function SQL(dbName, version, decision, size){
+	//打开数据库
+	this.db = openDatabase(dbName, version, decision, size);
 
-//设置错误回调
-SQL.setErrCallback = function(callback){
-	SQL.errCallback = callback;
-};
-//设置成功回调
-SQL.setSuccessCallback = function(callback){
-	SQL.successCallback = callback;
-};
+	//设置错误回调
+	this.setErrCallback = function(callback){
+		this.errCallback = callback;
+	};
+	//设置成功回调
+	this.setSuccessCallback = function(callback){
+		this.successCallback = callback;
+	};
 
-//创建表
-SQL.createTable = function(tName, value, errCallback = SQL.errCallback, successCallback = SQL.successCallback){
-	if (!SQL.db)
-		console.error("请先打开数据库");
-	
-	SQL.db.transaction(function (tx){
-		tx.executeSql(`CREATE TABLE IF NOT EXISTS ${tName} (${ value.join(",") })`);
-	}, errCallback, successCallback);
-};
-//删除表
-SQL.createTable = function(tName, errCallback = SQL.errCallback, successCallback = SQL.successCallback){
-	if (!SQL.db)
-		console.error("请先打开数据库");
-	
-	SQL.db.transaction(function (tx){
-		tx.executeSql(`DROP TABLE ${tName}`);
-	}, errCallback, successCallback);
-};
+	//删除数据库
+	this.deleteDatabase = function(dbName, errCallback = this.errCallback, successCallback = this.successCallback){
+		this.db.transaction(function (tx){
+			tx.executeSql(`DROP DATABASE ${dbName}`);
+		}, errCallback, successCallback);
+	};
 
-//插入数据
-SQL.insertData = function(tName, dataName, data, errCallback = SQL.errCallback, successCallback = SQL.successCallback){
-	if (!SQL.db)
-		console.error("请先打开数据库");
-	
-	SQL.db.transaction(function (tx){
-		tx.executeSql(`INSERT INTO ${tName} (${ dataName.join(",") }) VALUES (${ data.join(",") })`);
-	}, errCallback, successCallback);
-};
-//读取数据
-SQL.selectData = function(tName, dataName=["*"], readCallback, errCallback = SQL.errCallback, successCallback = SQL.successCallback){
-	if (!SQL.db)
-		console.error("请先打开数据库");
-	
-	SQL.db.transaction(function (tx){
-		tx.executeSql(`SELECT ${ dataName.join(",") } FROM ${tName}`,[],function(tx, data){
-			readCallback(data.rows);
-		});
-	}, errCallback, successCallback);
-};
-//删除数据
-SQL.deleteData = function(tName, where, errCallback = SQL.errCallback, successCallback = SQL.successCallback){
-	if (!SQL.db)
-		console.error("请先打开数据库");
-	
-	SQL.db.transaction(function (tx){
-		var sql = `DELETE FROM ${tName}`;
-		if (where)
-			sql += ` WHERE ${where}`;
-		tx.executeSql(sql);
-	}, errCallback, successCallback);
-};
-//更新数据
-SQL.deleteData = function(tName, set, where, errCallback = SQL.errCallback, successCallback = SQL.successCallback){
-	if (!SQL.db)
-		console.error("请先打开数据库");
-	
-	SQL.db.transaction(function (tx){
-		set = set.map(function(value){
-			return value.join("=");
-		}).join(",");
-		var sql = `UPDATE ${tName} SET ${set}`;
-		if (where)
-			sql += ` WHERE ${where}`;
-		tx.executeSql(sql);
-	}, errCallback, successCallback);
-};
+	//创建表
+	this.createTable = function(tName, value, errCallback = this.errCallback, successCallback = this.successCallback){
+		this.db.transaction(function (tx){
+			tx.executeSql(`CREATE TABLE IF NOT EXISTS ${tName} (${ value.join(",") })`);
+		}, errCallback, successCallback);
+	};
+	//删除表
+	this.createTable = function(tName, errCallback = this.errCallback, successCallback = this.successCallback){
+		this.db.transaction(function (tx){
+			tx.executeSql(`DROP TABLE ${tName}`);
+		}, errCallback, successCallback);
+	};
+
+	//插入数据
+	this.insertData = function(tName, dataName, data, errCallback = this.errCallback, successCallback = this.successCallback){
+		this.db.transaction(function (tx){
+			tx.executeSql(`INSERT INTO ${tName} (${ dataName.join(",") }) VALUES (${ data.join(",") })`);
+		}, errCallback, successCallback);
+	};
+	//读取数据
+	this.selectData = function(tName, dataName=["*"], readCallback, errCallback = this.errCallback, successCallback = this.successCallback){
+		this.db.transaction(function (tx){
+			tx.executeSql(`SELECT ${ dataName.join(",") } FROM ${tName}`,[],function(tx, data){
+				readCallback(data.rows);
+			});
+		}, errCallback, successCallback);
+	};
+	//删除数据
+	this.deleteData = function(tName, where, errCallback = this.errCallback, successCallback = this.successCallback){
+		this.db.transaction(function (tx){
+			var sql = `DELETE FROM ${tName}`;
+			if (where)
+				sql += ` WHERE ${where}`;
+			tx.executeSql(sql);
+		}, errCallback, successCallback);
+	};
+	//更新数据
+	this.deleteData = function(tName, set, where, errCallback = this.errCallback, successCallback = this.successCallback){
+		this.db.transaction(function (tx){
+			set = set.map(function(value){
+				return value.join("=");
+			}).join(",");
+			var sql = `UPDATE ${tName} SET ${set}`;
+			if (where)
+				sql += ` WHERE ${where}`;
+			tx.executeSql(sql);
+		}, errCallback, successCallback);
+	};
+}
