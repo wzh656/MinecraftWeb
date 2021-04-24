94596c7a80b81adb5fbd4f698882d0a893314038
wzh656 <60345146+wzh656@users.noreply.github.com>
2021-04-24 13:25:02 +0800

    .

--- js/modules/js+.js
+++ js/modules/js+.js
@@ -53,6 +53,12 @@ Date.prototype.format = function(fmt){
 	return fmt;	 
 }
 
+function GetQueryString(name){
+	var reg = new RegExp("(^|&)"+ name +"=([^&]*)(&|$)");
+	var r = window.location.search.substr(1).match(reg);
+	if(r!=null)return  unescape(r[2]); return null;
+}
+
 // 保留小数位数
 {
 	const round = Math.round;
