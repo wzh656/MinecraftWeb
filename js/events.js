4bb94b02b6a4142f7d8d16071f8acd2164f004f6
wzh656 <1826632591@qq.com>
2021-07-02 17:03:34 +0800

    挖掘

--- js/events.js
+++ js/events.js
@@ -12,6 +12,34 @@ const Events = {
 		const obj = ray2D().filter(obj => obj.object instanceof THREE.Mesh)[0]; //Mesh物体
 		if (!obj) return;
 		
+		let direction; //挖掘方向
+		switch (obj.faceIndex){
+			case 0:
+			case 1:
+				direction = "x+";
+				break;
+			case 2:
+			case 3:
+				direction = "x-";
+				break;
+			case 4:
+			case 5:
+				direction = "y+";
+				break;
+			case 6:
+			case 7:
+				direction = "y-";
+				break;
+			case 8:
+			case 9:
+				direction = "z+";
+				break;
+			case 10:
+			case 11:
+				direction = "z-";
+				break;
+		}
+		
 		const thing = obj.object.userData.thingObject, //物体对象
 			hold = deskgood.hold[deskgood.choice]; //挖掘工具
 		
@@ -21,39 +49,51 @@ const Events = {
 		) return; //距离**2 >= 手长**2 单位:px=cm
 		
 		let free, //手上空闲位置
-			digSpeed; //挖掘速度 单位: cm³/s
+			digSpeed, //挖掘速度 单位: cm³/s
+			take; //拿走的方块
+		//判断hold类型
 		if (hold == null){
 			free = deskgood.choice;
 			digSpeed = thing.get("attr", "block", "digSpeed", "手");
 			
-		}else if (hold.type == "Entity"){ //实体无法用于挖掘
+		//实体无法用于挖掘
+		}else if (hold.type == "Entity"){
 			return print("当前工具无法挖掘该方块", "无法挖掘", 3, "#f00");
 			
-		}else if (hold.type == "Tool"){ //工具挖掘
-			free = deskgood.hold.indexOf(null);
-			if (free == -1)
-				return print("东西太多，我拿不下了", "拿不下方块", 3, "#f00");
+		//用工具挖掘
+		}else if (hold.type == "Tool"){
+			const left = deskgood.hold[deskgood.choice-1];
+			if (left &&
+				hold.name == left.name && //同种方块
+				hold.get("attr", "stackable") == true //可叠加
+			){ //放在左边
+				free = deskgood.choice - 1;
+				take = left;
+			}else{ //放在空位
+				free = deskgood.hold.indexOf(null);
+				if (free == -1)
+					return print("东西太多，我拿不下了", "拿不下方块", 3, "#f00");
+			}
 			digSpeed = thing.get("attr", "block", "digSpeed", hold.name);
 			
-		}else{ //方块或实体方块
+		//方块或实体方块
+		}else{
 			if (hold.name != thing.name || hold.get("attr", "stackable") != true) //非同种物体 或 不可叠加
 				return print("当前工具无法挖掘该方块", "无法挖掘", 3, "#f00");
+			
 			if (deskgood.hold.indexOf(null) == -1) //空出手来挖
-				return print("东西太多，我没手挖了", "没手挖方块", 3, "#f00");
+				return print("东西太多，我没办法空出手来挖了", "没手挖方块", 3, "#f00");
+			
 			free = deskgood.choice; //挖到叠加
 			digSpeed = thing.get("attr", "block", "digSpeed", "手"); //用手挖掘
-			if (hold.type == "Block") //方块 变为 实体方块
+			take = hold;
+			if (hold.type == "Block") //要叠加的方块 转化为 实体方块
 				hold = deskgood.hold[deskgood.choice] = hold.toEntityBlock();
 		}
-		
 		if (!digSpeed)
 			return print("当前工具无法挖掘该方块", "无法挖掘", 3, "#f00");
 		
-		this.digging = true; //正在挖掘
-		
-		console.log("startDig", thing, hold, "digSpeed:", digSpeed*time.getSpeed(), "cm³/s, ", 1e6/digSpeed/time.getSpeed(), "s/cm")
-		
-		//转化为实体方块
+		//挖掘的物体 转化为 实体方块
 		let entityBlock;
 		switch (thing.type){
 			case "Block": //方块 转化为实体方块
@@ -68,28 +108,39 @@ const Events = {
 			case "Entity": //实体
 				break;
 		}
+		entityBlock.set("attr","entityBlock","size","y1", entityBlock.get("attr","entityBlock","size","y1")||100);
+		
+		this.digging = true; //正在挖掘
+		console.log("startDig", thing, hold, "digSpeed:", digSpeed*time.getSpeed(), "cm³/s, ", 1e6/digSpeed/time.getSpeed(), "s/cm")
 		
 		//处理事件
 		if ( eval(thing.get("attr", "block", "onStartDig")) === false ) return;
 		
 		//挖掘
-		entityBlock.set("attr","entityBlock","size","y1", entityBlock.get("attr","entityBlock","size","y1")||100);
-		let take = hold; //拿走的方块
+		let take;
 		this.digId = time.setInterval(()=>{
 			
 			if (!take){
-				take = entityBlock.cloneAttr();
-				take.set("attr","entityBlock","size","y1", 0);
+				const digGet = entityBlock.get("attr", "block", "digGet");
+				if (digGet){
+					take = new EntityBlock({
+						name: entityBlock.get("attr", "block", "digGet"), //挖掘获得
+						attr: entityBlock.get("attr")
+					});
+				}else{
+					take = entityBlock.cloneAttr();
+				}
+				take.set("attr","entityBlock","size", direction, 0);
 				deskgood.hold.addOne(take, free); //克隆一个放在手中
 			}
 			
-			entityBlock.attr.entityBlock.size.y1 -= 1;
-			take.attr.entityBlock.size.y1 += 1;
+			entityBlock.attr.entityBlock.size["direction"]--;
+			take.attr.entityBlock.size["direction"]++;
 			
-			console.log("Digging", entityBlock.attr.entityBlock.size.y1, take.attr.entityBlock.size.y1)
+			console.log("Digging", entityBlock.attr.entityBlock.size["direction"], take.attr.entityBlock.size["direction"])
 			entityBlock.updateSize(); //更新大小
 			
-			if (entityBlock.attr.entityBlock.size.y1 <= 0){ //挖掘结束
+			if (entityBlock.attr.entityBlock.size["direction"] <= 0){ //挖掘结束
 				console.log("endDig")
 				time.clearInterval(this.digId);
 				this.digging = false; //挖掘结束
@@ -116,9 +167,8 @@ const Events = {
 		const thing = obj.object.userData.thingObject; //物体对象
 		
 		//处理事件
-		if ( thing &&
-			eval(thing.get("attr", "block", "onEndDig")) === false
-		) return;
+		if ( eval(thing.get("attr", "block", "onEndDig")) === false )
+			return;
 		
 		console.log("endDig")
 		this.digging = false; //挖掘结束
@@ -176,8 +226,6 @@ const Events = {
 			case 11:
 				pos.z--;
 				break;
-			default:
-				throw ["faceIndex wrong:", obj.faceIndex];
 		}
 		
 		//是否超出手长
