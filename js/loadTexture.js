74579272d851b16fc6205b80292cd377f615f3a2
wzh <60345146+wzh656@users.noreply.github.com>
2022-08-21 16:16:50 +0800

    Update loadTexture.js

--- js/loadTexture.js
+++ js/loadTexture.js
@@ -2,7 +2,7 @@
 let TEXTURES;
 async function loadTextures(){
 	const keys = Object.keys(TEMPLATES);
-	TEXTURES = Img.grid(await Img.get("https://pic.imgdb.cn/item/6301e44916f2c2beb153f9a0.png"), 16, 16);
+	TEXTURES = Img.grid(await Img.get("./img/textures/textures.png"), 16, 16);
 	for (const i of keys.slice(1)){ //除去空气
 		const block = TEMPLATES[i];
 		for (let j=0; j<block.block.face.length; j++){
